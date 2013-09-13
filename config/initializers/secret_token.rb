3fa1e56d641b0b7feac45b9f25e5b24fcc90787e
kdurr <kristina.l.durr@gmail.com>
2013-09-13 15:30:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogHub::Application.config.secret_key_base = '44a86cd4fa6b5961e5f92d9d84d0faeaae733da296b49b0a7b3f739e2c87f48384ec0dacf35d0129c10cf4991b5683bf503a403844602ddfc36bfd9aab31c79e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DogHub::Application.config.session_store :cookie_store, key: '_DogHub_session'
